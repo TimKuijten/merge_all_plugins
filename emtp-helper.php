ea869543f8aa86bc74e629a46a8e4e010a162c51
TimKuijten <121023453+TimKuijten@users.noreply.github.com>
2025-08-30 18:17:41 +0200

    Add multi-account SMTP support and account selection for bulk emails

--- emtp-helper.php
+++ emtp-helper.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Plugin Name: SMTP Helper
- * Description: Simple plugin to configure SMTP settings for WordPress.
- * Version: 1.0.0
+ * Description: Simple plugin to configure multiple SMTP accounts for WordPress.
+ * Version: 1.1.0
  * Author: ChatGPT
  */
 
@@ -10,6 +10,9 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
+/**
+ * Add settings page.
+ */
 function smtp_helper_add_menu() {
     add_options_page(
         'SMTP Helper',
@@ -21,76 +24,149 @@ function smtp_helper_add_menu() {
 }
 add_action('admin_menu', 'smtp_helper_add_menu');
 
+/**
+ * Register option for multiple accounts.
+ */
 function smtp_helper_register_settings() {
-    register_setting('smtp_helper_options_group', 'smtp_helper_options', 'smtp_helper_sanitize');
+    register_setting('smtp_helper_options_group', 'smtp_helper_accounts', 'smtp_helper_sanitize_accounts');
 }
 add_action('admin_init', 'smtp_helper_register_settings');
 
+/**
+ * Helper to fetch all accounts.
+ *
+ * @return array
+ */
+function smtp_helper_get_accounts() {
+    $accounts = get_option('smtp_helper_accounts', array());
+    return is_array($accounts) ? $accounts : array();
+}
+
+/**
+ * Render the settings page.
+ */
 function smtp_helper_render_settings_page() {
-    $options = get_option('smtp_helper_options', array());
+    $accounts = smtp_helper_get_accounts();
+    if (empty($accounts)) {
+        $accounts = array(array());
+    }
     ?>
     <div class="wrap">
         <h1><?php esc_html_e('SMTP Helper Settings', 'smtp-helper'); ?></h1>
         <form method="post" action="options.php">
             <?php settings_fields('smtp_helper_options_group'); ?>
-            <table class="form-table" role="presentation">
-                <tr>
-                    <th scope="row"><label for="smtp_helper_host"><?php esc_html_e('SMTP Host', 'smtp-helper'); ?></label></th>
-                    <td><input name="smtp_helper_options[host]" id="smtp_helper_host" type="text" value="<?php echo esc_attr($options['host'] ?? ''); ?>" class="regular-text" /></td>
-                </tr>
-                <tr>
-                    <th scope="row"><label for="smtp_helper_port"><?php esc_html_e('SMTP Port', 'smtp-helper'); ?></label></th>
-                    <td><input name="smtp_helper_options[port]" id="smtp_helper_port" type="number" value="<?php echo esc_attr($options['port'] ?? ''); ?>" class="small-text" /></td>
-                </tr>
-                <tr>
-                    <th scope="row"><label for="smtp_helper_encryption"><?php esc_html_e('Encryption', 'smtp-helper'); ?></label></th>
-                    <td>
-                        <select name="smtp_helper_options[encryption]" id="smtp_helper_encryption">
-                            <?php $enc = $options['encryption'] ?? ''; ?>
-                            <option value="" <?php selected($enc, ''); ?>><?php esc_html_e('None', 'smtp-helper'); ?></option>
-                            <option value="ssl" <?php selected($enc, 'ssl'); ?>>SSL</option>
-                            <option value="tls" <?php selected($enc, 'tls'); ?>>TLS</option>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row"><label for="smtp_helper_username"><?php esc_html_e('Username', 'smtp-helper'); ?></label></th>
-                    <td><input name="smtp_helper_options[username]" id="smtp_helper_username" type="text" value="<?php echo esc_attr($options['username'] ?? ''); ?>" class="regular-text" /></td>
-                </tr>
-                <tr>
-                    <th scope="row"><label for="smtp_helper_password"><?php esc_html_e('Password', 'smtp-helper'); ?></label></th>
-                    <td><input name="smtp_helper_options[password]" id="smtp_helper_password" type="password" value="<?php echo esc_attr($options['password'] ?? ''); ?>" class="regular-text" /></td>
-                </tr>
-                <tr>
-                    <th scope="row"><label for="smtp_helper_from_email"><?php esc_html_e('From Email', 'smtp-helper'); ?></label></th>
-                    <td><input name="smtp_helper_options[from_email]" id="smtp_helper_from_email" type="email" value="<?php echo esc_attr($options['from_email'] ?? ''); ?>" class="regular-text" /></td>
-                </tr>
-                <tr>
-                    <th scope="row"><label for="smtp_helper_from_name"><?php esc_html_e('From Name', 'smtp-helper'); ?></label></th>
-                    <td><input name="smtp_helper_options[from_name]" id="smtp_helper_from_name" type="text" value="<?php echo esc_attr($options['from_name'] ?? ''); ?>" class="regular-text" /></td>
-                </tr>
-            </table>
+            <div id="smtp-helper-accounts">
+                <?php foreach ($accounts as $i => $acc) : ?>
+                <fieldset class="smtp-helper-account" style="border:1px solid #ccc;padding:10px;margin-bottom:10px;">
+                    <legend><?php printf(esc_html__('Account %d', 'smtp-helper'), $i + 1); ?></legend>
+                    <table class="form-table" role="presentation">
+                        <tr>
+                            <th scope="row"><label for="smtp_helper_host_<?php echo $i; ?>"><?php esc_html_e('SMTP Host', 'smtp-helper'); ?></label></th>
+                            <td><input name="smtp_helper_accounts[<?php echo $i; ?>][host]" id="smtp_helper_host_<?php echo $i; ?>" type="text" value="<?php echo esc_attr($acc['host'] ?? ''); ?>" class="regular-text" /></td>
+                        </tr>
+                        <tr>
+                            <th scope="row"><label for="smtp_helper_port_<?php echo $i; ?>"><?php esc_html_e('SMTP Port', 'smtp-helper'); ?></label></th>
+                            <td><input name="smtp_helper_accounts[<?php echo $i; ?>][port]" id="smtp_helper_port_<?php echo $i; ?>" type="number" value="<?php echo esc_attr($acc['port'] ?? ''); ?>" class="small-text" /></td>
+                        </tr>
+                        <tr>
+                            <th scope="row"><label for="smtp_helper_encryption_<?php echo $i; ?>"><?php esc_html_e('Encryption', 'smtp-helper'); ?></label></th>
+                            <td>
+                                <?php $enc = $acc['encryption'] ?? ''; ?>
+                                <select name="smtp_helper_accounts[<?php echo $i; ?>][encryption]" id="smtp_helper_encryption_<?php echo $i; ?>">
+                                    <option value="" <?php selected($enc, ''); ?>><?php esc_html_e('None', 'smtp-helper'); ?></option>
+                                    <option value="ssl" <?php selected($enc, 'ssl'); ?>>SSL</option>
+                                    <option value="tls" <?php selected($enc, 'tls'); ?>>TLS</option>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row"><label for="smtp_helper_username_<?php echo $i; ?>"><?php esc_html_e('Username', 'smtp-helper'); ?></label></th>
+                            <td><input name="smtp_helper_accounts[<?php echo $i; ?>][username]" id="smtp_helper_username_<?php echo $i; ?>" type="text" value="<?php echo esc_attr($acc['username'] ?? ''); ?>" class="regular-text" /></td>
+                        </tr>
+                        <tr>
+                            <th scope="row"><label for="smtp_helper_password_<?php echo $i; ?>"><?php esc_html_e('Password', 'smtp-helper'); ?></label></th>
+                            <td><input name="smtp_helper_accounts[<?php echo $i; ?>][password]" id="smtp_helper_password_<?php echo $i; ?>" type="password" value="<?php echo esc_attr($acc['password'] ?? ''); ?>" class="regular-text" /></td>
+                        </tr>
+                        <tr>
+                            <th scope="row"><label for="smtp_helper_from_email_<?php echo $i; ?>"><?php esc_html_e('From Email', 'smtp-helper'); ?></label></th>
+                            <td><input name="smtp_helper_accounts[<?php echo $i; ?>][from_email]" id="smtp_helper_from_email_<?php echo $i; ?>" type="email" value="<?php echo esc_attr($acc['from_email'] ?? ''); ?>" class="regular-text" /></td>
+                        </tr>
+                        <tr>
+                            <th scope="row"><label for="smtp_helper_from_name_<?php echo $i; ?>"><?php esc_html_e('From Name', 'smtp-helper'); ?></label></th>
+                            <td><input name="smtp_helper_accounts[<?php echo $i; ?>][from_name]" id="smtp_helper_from_name_<?php echo $i; ?>" type="text" value="<?php echo esc_attr($acc['from_name'] ?? ''); ?>" class="regular-text" /></td>
+                        </tr>
+                    </table>
+                </fieldset>
+                <?php endforeach; ?>
+            </div>
+            <p><button type="button" class="button" id="smtp-helper-add-account"><?php esc_html_e('Add Account', 'smtp-helper'); ?></button></p>
             <?php submit_button(); ?>
         </form>
     </div>
+    <script>
+    document.getElementById('smtp-helper-add-account').addEventListener('click', function(){
+        const container = document.getElementById('smtp-helper-accounts');
+        const index = container.querySelectorAll('.smtp-helper-account').length;
+        const template = container.querySelector('.smtp-helper-account');
+        if(!template) return;
+        const clone = template.cloneNode(true);
+        clone.querySelectorAll('input,select,label').forEach(function(el){
+            if(el.tagName === 'LABEL'){
+                const attr = el.getAttribute('for');
+                if(attr) el.setAttribute('for', attr.replace(/_\d+$/, '_' + index));
+            } else {
+                const name = el.getAttribute('name');
+                if(name) el.setAttribute('name', name.replace(/\[\d+\]/, '[' + index + ']'));
+                const id = el.getAttribute('id');
+                if(id) el.setAttribute('id', id.replace(/_\d+$/, '_' + index));
+                el.value = '';
+            }
+        });
+        container.appendChild(clone);
+    });
+    </script>
     <?php
 }
 
-function smtp_helper_sanitize($input) {
-    $output = array();
-    $output['host']       = sanitize_text_field($input['host'] ?? '');
-    $output['port']       = intval($input['port'] ?? 0);
-    $output['encryption'] = sanitize_text_field($input['encryption'] ?? '');
-    $output['username']   = sanitize_text_field($input['username'] ?? '');
-    $output['password']   = sanitize_text_field($input['password'] ?? '');
-    $output['from_email'] = sanitize_email($input['from_email'] ?? '');
-    $output['from_name']  = sanitize_text_field($input['from_name'] ?? '');
-    return $output;
+/**
+ * Sanitize accounts before saving.
+ *
+ * @param array $input
+ * @return array
+ */
+function smtp_helper_sanitize_accounts($input) {
+    $out = array();
+    if (is_array($input)) {
+        foreach ($input as $acc) {
+            $o = array();
+            $o['host']       = sanitize_text_field($acc['host'] ?? '');
+            $o['port']       = intval($acc['port'] ?? 0);
+            $o['encryption'] = sanitize_text_field($acc['encryption'] ?? '');
+            $o['username']   = sanitize_text_field($acc['username'] ?? '');
+            $o['password']   = sanitize_text_field($acc['password'] ?? '');
+            $o['from_email'] = sanitize_email($acc['from_email'] ?? '');
+            $o['from_name']  = sanitize_text_field($acc['from_name'] ?? '');
+            $out[] = $o;
+        }
+    }
+    return $out;
 }
 
+/**
+ * Configure PHPMailer using selected account.
+ */
 function smtp_helper_phpmailer_init($phpmailer) {
-    $options = get_option('smtp_helper_options');
-    if (empty($options) || empty($options['host'])) {
+    $accounts = smtp_helper_get_accounts();
+    if (empty($accounts)) {
+        return;
+    }
+    $index = apply_filters('smtp_helper_selected_account_index', 0);
+    if (!isset($accounts[$index])) {
+        $index = 0;
+    }
+    $options = $accounts[$index];
+
+    if (empty($options['host'])) {
         return;
     }
 
@@ -112,3 +188,4 @@ function smtp_helper_phpmailer_init($phpmailer) {
     }
 }
 add_action('phpmailer_init', 'smtp_helper_phpmailer_init');
+
